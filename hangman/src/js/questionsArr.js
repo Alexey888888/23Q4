b9264904b850d122b372b9e4c4a085c10956a4d7
Alexey888888 <brdkmail@gmail.com>
2024-01-13 21:26:19 +0200

    refactor: change some hints and secret words

--- hangman/src/js/questionsArr.js
+++ hangman/src/js/questionsArr.js
@@ -1,11 +1,11 @@
 const questions = [
-  { hint: 'The longest river in the world', answer: 'Amazon' },
+  { hint: 'The highest mountain in the world', answer: 'Everest' },
   { hint: 'The capital of Belarus', answer: 'Minsk' },
   { hint: 'A device used for eating', answer: 'spoon' },
   { hint: 'Means of connection', answer: 'phone' },
   { hint: 'Shines at night', answer: 'moon' },
   { hint: 'Every home has it on the map', answer: 'address' },
-  { hint: 'Two-wheel means of transport', answer: 'motorcycle' },
+  { hint: 'The largest and deepest ocean on Earth', answer: 'Pacific' },
   { hint: 'Solar system’s biggest star', answer: 'sun' },
   { hint: 'The most popular sport in the world', answer: 'football' },
   { hint: 'Predator swimming in the river', answer: 'crocodile' },
