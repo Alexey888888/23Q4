e647f18f1df65239745d427a659f10ff0565d6a3
Alexey888888 <brdkmail@gmail.com>
2024-01-25 22:25:09 +0200

    feat: implement class Node for html tags generation, generate base layout with js

---
+++ nonograms/src/js/classNode.js
@@ -0,0 +1,13 @@
+export class Node {
+  constructor({
+    tagName = "div",
+    classNames = [],
+    texContent = "",
+    parentNode,
+  }) {
+    this.node = document.createElement(tagName);
+    this.node.classList.add(...classNames);
+    this.node.textContent = texContent;
+    if (parentNode) parentNode.append(this.node);
+  }
+}
---
+++ nonograms/src/js/main.js
@@ -0,0 +1,51 @@
+import { Node } from "./classNode.js";
+
+const body = document.body;
+
+body.innerHTML = ""; //------REMOVE
+
+const header = new Node({
+  tagName: "header",
+  classNames: ["header"],
+  parentNode: body,
+});
+const background = new Node({
+  tagName: "img",
+  classNames: ["main-background"],
+  parentNode: header.node,
+});
+background.node.src = "./src/img/png/nonograms.png";
+const main = new Node({
+  tagName: "main",
+  classNames: ["main"],
+  parentNode: body,
+});
+const gameBox = new Node({ classNames: ["game-box"], parentNode: main.node });
+const gameBoxWrapper = new Node({
+  classNames: ["game-box__wrapper"],
+  parentNode: gameBox.node,
+});
+const toolsUp = new Node({
+  classNames: ["tools-up"],
+  parentNode: gameBoxWrapper.node,
+});
+const toolsUpWrapper = new Node({
+  className: ["tools-up__wrapper"],
+  parentNode: toolsUp.node,
+});
+const gameBoard = new Node({
+  classNames: ["game-board"],
+  parentNode: gameBoxWrapper.node,
+});
+const gameBoardWrapper = new Node({
+  classNames: ["game-board__wrapper"],
+  parentNode: gameBoard.node,
+});
+const toolsDown = new Node({
+  classNames: ["tools-down"],
+  parentNode: gameBoxWrapper.node,
+});
+const toolsDownWrapper = new Node({
+  className: ["tools-down__wrapper"],
+  parentNode: toolsDown.node,
+});
