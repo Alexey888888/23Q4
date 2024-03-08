c24af829b60190d612c33d9bc0dab6cea144850a
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-03-08 12:42:16 +0200

    chore: RSS-PZ-00 add Prettier, ESLint, Husky, TS, Webpack, modern-normalize (#64)
    * chore: add Prettier
    
    * chore: add ESLint
    
    * chore: add Husky
    
    * chore: add Webpack and TS
    
    * chore: change ESLint settings
    
    * chore: add modern-normalize

---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const prodConfig = require('./webpack.prod.config');
+const devConfig = require('./webpack.dev.config');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new EslintPlugin({ extensions: 'ts' }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets', 'img'),
+          to: path.resolve(__dirname, 'dist', 'assets', 'img'),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? prodConfig : devConfig;
+  return merge(baseConfig, envConfig);
+};
