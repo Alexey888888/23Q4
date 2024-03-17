3fb38f8091c20e233c16131a651b8c4e62c68de1
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-03-17 17:05:19 +0200

    feat: RSS-PZ-08 design main game page with interactive word cards (#78)

--- rss-puzzle/src/components/view/game/game.ts
+++ rss-puzzle/src/components/view/game/game.ts
@@ -1,14 +1,51 @@
+import './gameStyles.scss';
+
 import BaseComponent from '../../baseComponent';
+import wordCollectionLevel1 from '../../../data/word-collection/wordCollectionLevel1.json';
 
 export class Game {
-  placeholder: BaseComponent;
+  container: BaseComponent;
+
+  resultBlock: BaseComponent;
+
+  sourceBlock: BaseComponent;
 
   constructor() {
-    this.placeholder = new BaseComponent({ text: 'placeholder' });
+    this.resultBlock = new BaseComponent({ classNames: ['result-block'] });
+    this.sourceBlock = new BaseComponent({ classNames: ['source-block'] });
+    this.container = new BaseComponent(
+      { classNames: ['container'] },
+      new BaseComponent(
+        { classNames: ['game'] },
+        new BaseComponent({ classNames: ['game-wrapper'] }, this.resultBlock, this.sourceBlock),
+      ),
+    );
   }
 
   start() {
-    document.body.append(this.placeholder.getNode());
+    document.body.append(this.container.getNode());
+    this.fillSourceBlock();
+  }
+
+  fillSourceBlock() {
+    const resultRow = new BaseComponent({ classNames: ['result-row'] });
+    this.resultBlock.getNode().append(resultRow.getNode());
+    const wordsArr = wordCollectionLevel1.rounds[0].words[0].textExample.split(' ');
+    const shuffledWords = wordsArr.sort(() => Math.random() - 0.5);
+    shuffledWords.forEach((word) => {
+      const wordCard = new BaseComponent({ classNames: ['word'] });
+      const onClick = () => {
+        Game.fillResultBlock(wordCard, resultRow);
+        wordCard.removeListener('click', onClick);
+      };
+      wordCard.addListener('click', onClick);
+      wordCard.setTextContent(word);
+      this.sourceBlock.getNode().append(wordCard.getNode());
+    });
+  }
+
+  static fillResultBlock(wordCard: BaseComponent, resultRow: BaseComponent) {
+    resultRow.getNode().append(wordCard.getNode());
   }
 }
 
