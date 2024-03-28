e779fb382b49cd420b9008933333bf5be912231a
Alexey888888 <brdkmail@gmail.com>
2024-03-28 12:23:16 +0200

    refactor: refactor Utils class

--- async-race/src/utils/utils.ts
+++ async-race/src/utils/utils.ts
@@ -1,8 +1,13 @@
 import carBrands from '../components/view/garage/carBrands';
 
 export default class Utils {
+  static getRandomValue(maxValue: number) {
+    return Math.floor(Math.random() * maxValue);
+  }
+
   static getRandomColor(): string {
-    const getColorChannel = (): number => Math.floor(Math.random() * 256);
+    const colorPaletteNum = 256;
+    const getColorChannel = (): number => this.getRandomValue(colorPaletteNum);
     const toHex = (color: number): string => {
       return color.toString(16).padStart(2, '0');
     };
@@ -11,9 +16,9 @@ export default class Utils {
 
   static getRandomCarName(): string {
     const carNames = Object.keys(carBrands);
-    const randomCarNameIndex = Math.floor(Math.random() * carNames.length);
+    const randomCarNameIndex = this.getRandomValue(carNames.length);
     const randomCarName = carNames[randomCarNameIndex];
-    const randomCarModelIndex = Math.floor(Math.random() * carBrands[randomCarName].length);
+    const randomCarModelIndex = this.getRandomValue(carBrands[randomCarName].length);
     const randomCarModel = carBrands[randomCarName][randomCarModelIndex];
     return `${randomCarName} ${randomCarModel}`;
   }
